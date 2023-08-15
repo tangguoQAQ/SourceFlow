1077bc9d43f10c4b902f2e787a035019062c8444
tangguoQAQ <yanzicheng2007@outlook.com>
2023-08-15 13:02:05 +0800

    配置日志记录器，增加运行配置
    (cherry picked from commit eae1c1577efc76aad1609e4e92871828578aff5c)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module tgmori.sourceflow {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires org.jetbrains.annotations;
-
+	requires org.apache.logging.log4j;
 
 	opens tgmori.sourceflow to javafx.fxml;
 	exports tgmori.sourceflow;
--- src/main/java/tgmori/sourceflow/SourceFlow.java
+++ src/main/java/tgmori/sourceflow/SourceFlow.java
@@ -1,4 +1,15 @@
 package tgmori.sourceflow;
 
+import javafx.application.Application;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class SourceFlow {
+	public static final Logger LOGGER = LogManager.getLogger("SourceFlow");
+
+	public static void main(String[] args) {
+		SourceFlow.LOGGER.info("开始运行 Source Flow。");
+		Application.launch(SourceFlowApplication.class, args);
+		SourceFlow.LOGGER.info("结束运行 Source Flow。");
+	}
 }
